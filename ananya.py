96a4c55eb849d7ba3d1c6174c81e3069dfceb193
ananywaaaaa-byte <ananywaaaaa@gmail.com>
2026-06-22 16:24:54 +0530

    first commit

---
+++ ananya.py
@@ -0,0 +1,9 @@
+x=[1,2,3,4,5,6,7,8,9,10]
+def xyz(x):
+    for i in x:
+        if i %2 ==0:
+            print("number is even",i)
+        else:
+            print ("number is odd ",i)
+xyz(x)            
+                  
